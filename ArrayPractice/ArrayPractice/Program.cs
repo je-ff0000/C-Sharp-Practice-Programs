8cae6152632879a1b595e757b608b2da2420baaa
je-ff0000 <12345678+je-ff0000@users.noreply.github.com>
2026-02-14 23:46:03 +0530

    Add practice program for array basics

---
+++ ArrayPractice/ArrayPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] ar = new int[5];
+            Console.WriteLine("Enter the elements");
+
+            for(int i = 0; i < 5; i++)
+            {
+                ar[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Array elements are");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(ar[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
