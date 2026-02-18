7db094f063ab5f1324a1a8d45dbdf74a72b25cc4
je-ff0000 <12345678+je-ff0000@users.noreply.github.com>
2026-02-18 14:06:46 +0530

    Correct property name

--- Property/Property/Program.cs
+++ Property/Property/Program.cs
@@ -14,12 +14,11 @@ namespace Property
         static void Main(string[] args)
         {
             A ob1 = new A();
-            A ob2 = new A();
             Console.WriteLine("Enter two numbers");
             ob1.P1 = Convert.ToInt32(Console.ReadLine());
-            ob2.P1 = Convert.ToInt32(Console.ReadLine());
+            ob1.P2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Sum: {0}", ob1.P1 + ob2.P1);
+            Console.Write("Sum: {0}", ob1.P1 + ob1.P2);
 
             Console.ReadKey();
 
