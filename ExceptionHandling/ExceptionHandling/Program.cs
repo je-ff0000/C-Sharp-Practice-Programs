f72c6cb0fb47e6f576d48608827e8908d5b683f2
je-ff0000 <12345678+je-ff0000@users.noreply.github.com>
2026-02-18 16:47:58 +0530

    Add ExceptionHandling and Generics programs

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x;
+                Console.WriteLine("Enter age:");
+                x = Convert.ToInt32(Console.ReadLine());
+                if (x <= 0)
+                {
+                    throw new ArgumentException("Age cannot be less than zero");
+                }
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("In finally block");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class gen<X, Y>
+    {
+        public X a;
+        public Y b;
+        public gen(X i, Y j)
+        {
+            a = i;
+            b = j;
+        }
+    }
+    class A 
+    { 
+        static void Main(string[] args)
+        {
+            int n, m;
+
+            Console.WriteLine("Enter 2 numbers:");
+            n = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+
+            gen<int, int> obj = new gen<int, int>(n, m);
+            Console.WriteLine("sum is {0}", obj.a + obj.b);
+            gen<string, string> obj1 = new gen<string, string>("HI ", "Everyone");
+            Console.WriteLine("string is {0}", obj1.a + obj1.b);
+
+            Console.ReadKey();
+        }
+    }
+}
